2fbc2536cf5270d182a64adbb2b8d81da95377c6
Bharath Kummari <bharath.kummari@icloudlogic.com>
2025-11-27 13:46:45 +0530

    exception handling and logging in comments_builder

--- app/builders/comments_builder.py
+++ app/builders/comments_builder.py
@@ -7,34 +7,46 @@ from app.models.request.comments_request import CommentCreateRequest, CommentFet
 from app.accessors.comments_accessor import CommentAccessor
 from app.utils.sort_util import SortQuery
 
+import logging
+
+logger = logging.getLogger(__name__)
+
 class CommentBuilder:
     def __init__(self, session: AsyncSession, accessor: CommentAccessor | None = None):
         self.session = session
         self.comments_accessor = accessor or CommentAccessor()
 
     async def build_create(self, request: CommentCreateRequest) -> Comment:
-        if request.comment_id is None:
-            new_comment_id = uuid.uuid4()
-        else:
-            new_comment_id = request.comment_id
-            await self.comments_accessor.close_active_record(new_comment_id, self.session)
+        try:
+            if request.comment_id is None:
+                new_comment_id = uuid.uuid4()
+            else:
+                new_comment_id = request.comment_id
+                await self.comments_accessor.close_active_record(new_comment_id, self.session)
 
-        model = Comment(
-            comment_id=new_comment_id,
-            summary_id=request.summary_id,
-            content=request.content,
-            created_by=uuid.uuid4(), 
-            effective_from=datetime.now(timezone.utc),
-            effective_to=None
-        )
-
-        return await self.comments_accessor.insert(model, self.session)
-
+            model = Comment(
+                comment_id=new_comment_id,
+                summary_id=request.summary_id,
+                content=request.content,
+                #created_by=uuid.uuid4(), 
+                effective_from=datetime.now(timezone.utc),
+                effective_to=None
+            )
+            logger.debug(f"Creating comment {new_comment_id} linked to summary {model.summary_id}")
+            return await self.comments_accessor.insert(model, self.session)
+        except Exception as exception:
+            logger.error(f"Failed to create comment: {exception}", exc_info=True)
+            raise
 
     async def build_comments_fetch(self, filters: CommentFetchFilter, sort: SortQuery):
-        return await self.comments_accessor.fetch(
-            session=self.session,
-            filters=filters,
-            sort=sort
-        )
+        logger.debug(f"Fetching comments with filters={filters} sort={sort}")
+        try:
+            return await self.comments_accessor.fetch(
+                session=self.session,
+                filters=filters,
+                sort=sort
+            )
+        except Exception as exc:
+            logger.error(f"Failed to fetch comments: {exc}", exc_info=True)
+            raise
 
