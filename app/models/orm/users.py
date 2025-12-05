6b4da1edfa3e983861f0944208d306660e481878
Bharath Kummari <bharath.kummari@icloudlogic.com>
2025-12-05 15:11:48 +0530

    orm model type fix

--- app/models/orm/users.py
+++ app/models/orm/users.py
@@ -5,7 +5,7 @@ from uuid import UUID
 
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy import BigInteger, String, Boolean, DateTime
-from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.dialects.postgresql import UUID as PG_UUID
 from sqlalchemy.sql import text
 
 from app.models.orm.base import Base
@@ -18,7 +18,7 @@ class User(Base):
     __scd2__ = True
 
     user_sk: Mapped[int] = mapped_column(BigInteger, primary_key=True)
-    user_id: Mapped[UUID] = mapped_column(UUID(as_uuid=True), nullable=False)
+    user_id: Mapped[UUID] = mapped_column(PG_UUID(as_uuid=True), nullable=False)
 
     user_name: Mapped[Optional[str]] = mapped_column(String)
     user_email: Mapped[Optional[str]] = mapped_column(String)
@@ -26,23 +26,21 @@ class User(Base):
     last_name: Mapped[Optional[str]] = mapped_column(String)
 
     is_active: Mapped[bool] = mapped_column(
-        Boolean,
-        server_default=text("true"),
-        nullable=False
-        )
+        Boolean, server_default=text("true"), nullable=False
+    )
     is_admin: Mapped[Optional[bool]] = mapped_column(Boolean)
 
     entity_type_id: Mapped[Optional[int]] = mapped_column(BigInteger)
 
-    created_by: Mapped[Optional[UUID]] = mapped_column(UUID(as_uuid=True))
+    created_by: Mapped[Optional[UUID]] = mapped_column(PG_UUID(as_uuid=True))
 
     created_date: Mapped[datetime] = mapped_column(
         DateTime(timezone=False),
         server_default=text("CURRENT_TIMESTAMP"),
-        nullable=False
-        )
+        nullable=False,
+    )
 
-    modified_by: Mapped[Optional[UUID]] = mapped_column(UUID(as_uuid=True))
+    modified_by: Mapped[Optional[UUID]] = mapped_column(PG_UUID(as_uuid=True))
 
     effective_from: Mapped[Optional[datetime]] = mapped_column(DateTime(timezone=True))
     effective_to: Mapped[Optional[datetime]] = mapped_column(DateTime(timezone=True))
