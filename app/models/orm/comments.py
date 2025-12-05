670559060a685e3ee23b6b74f4d714562d20a77a
Bharath Kummari <bharath.kummari@icloudlogic.com>
2025-12-05 15:09:48 +0530

    type fixes

--- app/models/orm/comments.py
+++ app/models/orm/comments.py
@@ -1,15 +1,16 @@
-
 from __future__ import annotations
 from typing import Optional
 from datetime import datetime
+from uuid import UUID
 
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy import BigInteger, Text, Boolean, DateTime
-from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.dialects.postgresql import UUID as PG_UUID
 from sqlalchemy.sql import text
 from app.models.orm.base import Base
 
 from app.settings import settings
+
 
 class Comment(Base):
     __tablename__ = "comments"
@@ -17,18 +18,21 @@ class Comment(Base):
     __scd2__ = True
 
     comment_sk: Mapped[int] = mapped_column(BigInteger, primary_key=True)
-    comment_id: Mapped[UUID] = mapped_column(UUID(as_uuid=True), nullable=False, index=True)
-    summary_id: Mapped[Optional[str]] = mapped_column(UUID(as_uuid=True))
+    comment_id: Mapped[UUID] = mapped_column(
+        PG_UUID(as_uuid=True), nullable=False, index=True
+    )
+    summary_id: Mapped[Optional[UUID]] = mapped_column(PG_UUID(as_uuid=True))
     content: Mapped[Optional[str]] = mapped_column(Text)
-    is_active: Mapped[bool] = mapped_column(Boolean, server_default=text("true"), nullable=False)
-    created_by: Mapped[Optional[str]] = mapped_column(UUID(as_uuid=True))
+    is_active: Mapped[bool] = mapped_column(
+        Boolean, server_default=text("true"), nullable=False
+    )
+    created_by: Mapped[Optional[UUID]] = mapped_column(PG_UUID(as_uuid=True))
     created_date: Mapped[datetime] = mapped_column(
         DateTime(timezone=True),
         server_default=text("CURRENT_TIMESTAMP"),
         nullable=False,
     )
-    modified_by: Mapped[Optional[str]] = mapped_column(UUID(as_uuid=True))
+    modified_by: Mapped[Optional[UUID]] = mapped_column(PG_UUID(as_uuid=True))
     effective_from: Mapped[Optional[datetime]] = mapped_column(DateTime(timezone=True))
     effective_to: Mapped[Optional[datetime]] = mapped_column(DateTime(timezone=True))
     entity_type_id: Mapped[Optional[int]] = mapped_column(BigInteger)
-
