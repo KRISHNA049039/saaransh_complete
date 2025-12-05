78832693629c716d7fbf7897cf37597ccf0f58a2
Bharath Kummari <bharath.kummari@icloudlogic.com>
2025-12-05 21:53:25 +0530

    summary response class

--- app/models/response/summaries_response.py
+++ app/models/response/summaries_response.py
@@ -1,12 +1,26 @@
+from pydantic import BaseModel
+from typing import Optional, Dict, Any
 from datetime import datetime
 from uuid import UUID
-from pydantic import BaseModel
+
+from app.models.response.user_prompts_response import UserPromptsResponse
+
 
 class SummaryResponse(BaseModel):
-    comment_id: UUID
-    summary_id: UUID | None
-    content: str | None
-    created_date: datetime
-    is_active: bool
+    summary_sk: int
+    summary_id: UUID
+    content: Optional[str]
+    start_date: Optional[datetime]
+    end_date: Optional[datetime]
+    status_id: Optional[int]
+    meta_data: Optional[Dict[str, Any]]
+    is_active: Optional[bool]
+    created_date: Optional[datetime]
+    created_by: Optional[UUID]
+    modified_by: Optional[UUID]
+    effective_from: Optional[datetime]
+    effective_to: Optional[datetime]
+    entity_type_id: Optional[int]
 
-    model_config = { "from_attributes": True }
+    class Config:
+        from_attributes = True
